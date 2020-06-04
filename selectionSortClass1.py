1f55aa04143d77843727f0fd0cac42084fa0e5b2
Monte Knowles <Monte.Faison@seattlecolleges.edu>
2020-06-04 09:19:36 -0700

    Selection Sort Class 1

---
+++ selectionSortClass1.py
@@ -0,0 +1,31 @@
+
+#Make an array for the sorting of the selection
+
+array = [13, 4, 9, 5, 3, 16, 12]
+
+def selectionSort(array):
+
+	n = len(array)
+
+	for i in range(n): #<--Whatever the length of the array is how many times you are going to run the for loop.
+
+		#Initially assume the first element of the unsorted part as the minimum 
+
+		minimum = i
+
+		for j in range(i+1, n):
+
+			if (array[j] < array[minimum]): #<---Comparison operator 
+
+			minimum = j
+
+		# Swap the minimum element with the first element of the unsorted part. 
+
+		temp = array[i]
+		array[i] = array[minimum]
+		array[minimum] = temp
+
+	return array
+
+	print(selectionSort(array)) 
+
