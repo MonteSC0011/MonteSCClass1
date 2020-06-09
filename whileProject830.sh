2a0777b8c6de7a972240b8e9e10f843ee3e40be7
Monte Knowles <Monte.Faison@seattlecolleges.edu>
2020-06-09 09:23:33 -0700

    While Project for 830 class

---
+++ whileProject830.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -n "Print message?"
+
+valid=0
+
+while 
+[ $valid == 0 ]
+do
+	read ans
+	case $ans in
+	yes|YES|y|Y ) echo Will print the message
+		      echo The Message
+		      valid=1
+                      ;;
+
+        [nN][oO]    ) echo Will NOT print the message
+		      valid=1 ;;
+        *           ) echo Yes Or No of some form please ;; 
+	esac
+done 
